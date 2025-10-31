acc26533c0e0290e8bc79758c65a04eea827319f
Akash Sharma <akasharma2019@gmail.com>
2025-10-31 15:02:30 +0530

    return 0 for empty input

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -2,6 +2,9 @@ package com.incubyte;
 
 public class StringCalculator {
 	public int add(String numbers) {
+		if (numbers == null || numbers.isEmpty()) {
+			return 0;
+		}
 		throw new UnsupportedOperationException("Not implemented yet");
 	}
 }
